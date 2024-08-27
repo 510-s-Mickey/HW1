0b263257f7a3848a418e8ec5179a3a8ebc1c9430
LuFree <LUfansx@users.noreply.github.com>
2024-08-27 18:28:38 +0000

    edit test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -5,6 +5,6 @@ def test_add_pass():
     assert add(2, 3) == 5  # this test should succeed
 
 def test_add_fail():
-    assert add(2, 2) == 5  # this test should fail
+    assert add(2, 5) == 5  # this test should fail
 
 
