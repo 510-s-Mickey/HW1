53b9889877377e643ccf2d1713632aa573881d80
Chris <100252884+chris-hzc@users.noreply.github.com>
2024-09-06 15:22:47 +0000

    fail test

--- test_mycode.py
+++ test_mycode.py
@@ -5,7 +5,7 @@ def test_add_pass():
     assert add(2, 3) == 5  # this test should succeed
 
 # If we uncomment the test_add_fail function, the workflow test will fail.
-# def test_add_fail():
-#     assert add(2, 5) == 5  # this test should fail
+def test_add_fail():
+    assert add(2, 5) == 5  # this test should fail
 
 
